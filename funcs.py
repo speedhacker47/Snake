b4988f753d16bff7cc78ac06a1f89e0fdb7db9aa
speedhacker47 <lazy.coder2024@gmail.com>
2023-07-16 14:05:56 +0530

    snake head movement

--- funcs.py
+++ funcs.py
@@ -3,22 +3,36 @@ import sys
 
 pygame.init()
 
-def check_pressed_key():    # Self Explainatry             
+def check_pressed_key():
+    press , hold =  [False,False,False,False] , [False,False,False,False] #right , left , up , down
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:                
                 sys.exit()
         if event.type == pygame.KEYDOWN:  #check anyone pressed the key
             if event.key == pygame.K_UP:   #check for a specific Key
-                print("up")
-                return 'u'
+                press[2] = True
+                
             elif event.key == pygame.K_DOWN:
-                print('down')
-                return 'd'
+                press[3] = True
             elif event.key == pygame.K_LEFT:
-                print('left')
-                return 'l'
+                press[1] = True
             elif event.key == pygame.K_RIGHT:
-                print('right')
-                return 'r'
-            else:
-                 return None
+                press[0] = True
+    return press
+
+def hold_keys():
+    hold = [False,False,False,False]
+    for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+               sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+         hold[0] = True
+    elif keys[pygame.K_LEFT]:
+         hold[1] = True
+    elif keys[pygame.K_UP]:
+         hold[2] = True
+    elif keys[pygame.K_DOWN]:
+         hold[3] = True
+    return hold 
--- playable_snake_01.py
+++ playable_snake_01.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-from funcs import check_pressed_key   # i put some functions in other python file so that actual code is less messy
+from funcs import check_pressed_key
+from funcs import hold_keys   # i put some functions in other python file so that actual code is less messy
 
 #Variables
 width , height , border = 500,500 ,50
@@ -15,10 +16,11 @@ win = pygame.display.set_mode((width,height)) # store window in a variable
 
 FPS = 144
 clock = pygame.time.Clock()
-exit = False
 
-dir = 'r'
-speed = 50
+dim = 20 #snake dimension
+
+dir = 'right'  #Initial Direction of head
+speed = 5
 
 class square():              #MAking class so i can assign properties to variables like snake haead and tail
     def __init__(self,x,y):
@@ -29,22 +31,39 @@ head = square(border,border)                  # snake head with innitial positio
 
 def draw():                
     win.fill(black)
-    pygame.draw.rect(win,red,(head.x,head.y,20,20))              
+
+    pygame.draw.rect(win,red,(head.x,head.y,dim,dim)) #Drawing Snake HEad
+
+    pygame.draw.aaline(win,white,(border,-100),(border,height+100))  #Drawing Borders
+    pygame.draw.aaline(win,white,(-100,border),(width+100,border))
+    pygame.draw.aaline(win,white,(width-border,-100),(width-border,height+100))
+    pygame.draw.aaline(win,white,(-100,height-border),(width+100,height-border))             
+
     pygame.display.update()    #Update the drawing changes made since last time
 
 
 def run_game():         # Head function to run the game
     while True:         #this loop will continue until you press exit , breaking this loop cause closing the window
         clock.tick(60)
+        check_head_direction()
         update_snake_pos()      #first we will update snake pos 
         draw()                  # Then draw it
 
         
-def update_snake_pos():   # Update All whole snake's positions
+def update_snake_pos():
+    global dir
+    print(dir)
+    if dir == 'right' and head.x < width-border-dim : head.x += speed
+    elif dir == 'left' and head.x > border: head.x -= speed
+    elif dir == 'up' and head.y > border : head.y -= speed
+    elif dir == 'down' and head.y < height-border-dim : head.y += speed  
+
+def check_head_direction():
+    global dir
     k = check_pressed_key()
-    if k == 'r' and head.x < width-border : head.x += speed
-    elif k == 'l' and head.x > border: head.x -= speed
-    elif k == 'u' and head.y > border : head.y -= speed
-    elif k == 'd' and head.y < height-border : head.y += speed  
+    if k[0] : dir = 'right'
+    elif k[1] : dir = 'left'
+    elif k[2] : dir = 'up'
+    elif k[3] : dir = 'down'
 
 run_game() #Run
