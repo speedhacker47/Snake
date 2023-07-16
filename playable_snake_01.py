5a6d950181d25a1ae5f11bd73cd278b9bd9777c4
speedhacker47 <lazy.coder2024@gmail.com>
2023-07-16 23:20:48 +0530

    FINAL
    still have some bugs
    
    1) food can occur inside tails
    
    game can look more cool and professional but i dont want want to waste much time on it
    
    Took me approx 12 Hours to make it

--- playable_snake_01.py
+++ playable_snake_01.py
@@ -23,6 +23,8 @@ dim = 20 #snake dimension
 dir = 'right'  #Initial Direction of head
 speed = 20
 
+highest_score = 0
+
 class square():              #MAking class so i can assign properties to variables like snake haead and tail
     def __init__(self,x,y):
         self.x = x              # x,y are coordinate of square shape in 2D space , both value starts from zero from top left
@@ -44,12 +46,11 @@ def draw():
     pygame.draw.rect(win,green,(head.x,head.y,dim,dim))
     pygame.draw.rect(win,red,(food.x,food.y,dim,dim))
     pygame.draw.rect(win,white,(border,border,width-2*border,height-2*border),1)
+    create_and_draw_texts()
 
     for i in range(1,len(tails)):
         pygame.draw.rect(win,white,(tails[i].x,tails[i].y,dim,dim))
         pygame.draw.rect(win,blue,(tails[i].x,tails[i].y,dim,dim),1)  #tail border
-
-    pygame.display.update()
 
 def run_game():                 # Head function to run the game
     global speed_of_game         
@@ -60,17 +61,18 @@ def run_game():                 # Head function to run the game
         update_tail_pos()
         update_snake_pos()
         update_food()      #first we will update snake pos 
-        draw()
-        if check_game_over(): print ("GAME OVER !")
-        speed_of_game = 100                  # Then draw it
-
+        draw()              # Then draw it
+        #create_and_draw_texts()
+        check_game_over()
+        speed_of_game = 100                  
+        pygame.display.update() # Update the screen and make all changes since last update
         
 def update_snake_pos():          # Update the snake position by seeing situation
     global dir
-    if dir == 'right' and head.x < width-border-dim : head.x += speed
-    elif dir == 'left' and head.x > border: head.x -= speed
-    elif dir == 'up' and head.y > border : head.y -= speed
-    elif dir == 'down' and head.y < height-border-dim : head.y += speed  
+    if dir == 'right' and head.x <= width-border-dim : head.x += speed
+    elif dir == 'left' and head.x >= border: head.x -= speed
+    elif dir == 'up' and head.y >= border : head.y -= speed
+    elif dir == 'down' and head.y <= height-border-dim : head.y += speed  
 
 def check_head_direction():      #Controls direction of head i.e snake
     global dir
@@ -111,18 +113,44 @@ def hold_speed_increase():  # Just Extra feature
     elif (dir == 'up' and get[2]) or (dir == 'down' and get[3]):
         speed_of_game -= 40
 
-def check_game_over():  #Check for Game Over
+def check_game_over():
+    global tails ,t ,dir ,highest_score
+    over = False
+
     for i in range(2,len(tails)):  #CHeck if head is touching any of tails
         if (head.x,head.y) == (tails[i].x,tails[i].y):
-            return True
-            
-    if head.x >= width-border-dim or head.x <= border: #check snake is crossing border
-        if dir == 'right' or dir == 'left':
-            return True
-    if head.y <= border or head.y >= height-border-dim:
-        if dir == 'up' or dir == 'down':
-            print('some')
-            return True
-    return False
+            over = True      
 
-run_game() #Run
+    if head.x <= border-dim or head.x >= width-border: #check snake is crossing border
+        if dir == 'right' or dir == 'left':
+            over = True
+    elif head.y <= border-dim or head.y >= height-border:
+        if dir == 'up' or dir == 'down':
+            over = True
+
+    if over :            # it does WHat happens if game over 
+        font = pygame.font.SysFont('comicsans', 30, True)
+        for q in [5,4,3,2,1]:
+            draw()
+            text_2 = font.render("GAME OVER", 1,red)
+            win.blit(text_2,(width/3,height-40))
+            text_3 = font.render(str(q), 1,white)
+            win.blit(text_3,(width/2,0))
+            pygame.display.update()
+            pygame.time.delay(1000)
+
+        if highest_score < len(tails): highest_score = len(tails)
+        tails = [head]
+        head.x = head.y = border+2*dim
+        t = 1
+        dir = 'right'
+
+def create_and_draw_texts():            # WIll Handle texts on screen
+    font = pygame.font.SysFont('comicsans', 15, True)
+    text = font.render("Score "+str(len(tails)-1), 1,(255,255,255))
+    win.blit(text, (10,0))
+    text2 = font.render("Highest Score "+ str(highest_score), 1,(255,255,255))
+    win.blit(text2, (10,20))
+
+if __name__ == "__main__":
+    run_game() #Run
